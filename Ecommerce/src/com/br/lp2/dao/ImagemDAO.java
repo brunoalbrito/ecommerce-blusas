ee229d3ff61b00e333e1e03ddc9f157e37c1c04d
41514262 <41514262@093094-P31S405.macklabs.br>
2016-11-01 11:29:17 -0200

    ImagemDAO - Terminado - Ñ Testado

--- Ecommerce/src/com/br/lp2/dao/ImagemDAO.java
+++ Ecommerce/src/com/br/lp2/dao/ImagemDAO.java
@@ -12,9 +12,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import static java.util.Collections.list;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,7 +36,7 @@ public class ImagemDAO implements GenericDAO<Imagem> {
     public boolean insert(Imagem imagem) {
         boolean result = false;
 
-        String sql = "INSERT INTO imagem (id_produto,imagem)VALUES(?,?)";
+        String sql = "INSERT INTO imagem (id_produto,conteudo)VALUES(?,?)";
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setLong(1, imagem.getId_produto());
@@ -52,22 +56,88 @@ public class ImagemDAO implements GenericDAO<Imagem> {
 
     @Override
     public List<Imagem> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Imagem> imagens = new ArrayList<>();
+        String sql = "SELECT * FROM imagem";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Imagem imagem = new Imagem();
+                imagem.setId_imagem(rs.getLong("id_imagem"));
+                imagem.setConteudo(rs.getBytes("conteudo"));
+                imagem.setId_produto(rs.getLong("id_produto"));
+            }
+            rs.close();
+            ps.close();
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return imagens;
     }
 
     @Override
     public Imagem findById(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM imagem WHERE id_imagem = ?";
+        Imagem imagem = new Imagem();
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                imagem.setId_imagem(rs.getLong("id_imagem"));
+                Blob blob = rs.getBlob("CONTEUDO");
+                imagem.setConteudo(blob.getBytes(1, (int) blob.length()));
+                blob.free();
+                imagem.setId_produto(rs.getLong("id_produto"));
+            }
+            rs.close();
+            ps.close();
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return imagem;
     }
 
     @Override
-    public boolean modify(Imagem e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean modify(Imagem imagem) {
+        boolean result = false;
+        String sql = "UPDATE imagem SET conteudo = ? WHERE id_imagem = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setBlob(1, new SerialBlob(imagem.getConteudo()));
+
+            int resp = ps.executeUpdate();
+            if (resp != 0) {
+                result = true;
+            }
+            ps.close();
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
 
     @Override
-    public boolean remove(Imagem e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean remove(Imagem imagem) {
+        boolean result = false;
+        String sql = "DELETE FROM imagem WHERE id_imagem = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setLong(1, imagem.getId_imagem());
+
+            int resp = ps.executeUpdate();
+            result = (resp != 0);
+
+            ps.close();
+            connection.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
